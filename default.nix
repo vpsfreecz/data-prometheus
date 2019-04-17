74565628b3e69331f42def646deac4d107ada17b
Richard Marko <srk@48.io>
2019-04-17 20:19:04 +0200

    bump to ghc864

--- default.nix
+++ default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc863" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" }:
 nixpkgs.haskell.packages.${compiler}.callPackage ./data-prometheus.nix { }
