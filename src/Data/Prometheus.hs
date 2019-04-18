f25e9a2ef673207f3c958a3044675c5512a9e748
Richard Marko <srk@48.io>
2019-04-18 11:40:30 +0200

    add annotations, shave off some warnings

--- src/Data/Prometheus.hs
+++ src/Data/Prometheus.hs
@@ -17,22 +17,34 @@ import Data.Attoparsec.ByteString.Char8
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Map as M
 
+-- | Parse Prometheus metrics from ByteString
+parseProm :: B.ByteString -> Either String PromMetrics
 parseProm = parseOnly parseMetrics
 
+-- | Filter metrics where name is prefixed by `pattern`
+filterMetrics :: String -> M.Map MetricId a -> M.Map MetricId a
 filterMetrics pattern = M.filterWithKey (\k _ -> (B.pack pattern) `B.isPrefixOf` (name k))
 
+-- | Find metrics where name is equal to `pattern`
+findMetrics :: String -> M.Map MetricId a -> M.Map MetricId a
 findMetrics pattern = M.filterWithKey (\k _ -> (B.pack pattern) == (name k))
 
-hasLabel label = M.filterWithKey (\k _ -> (M.member (B.pack label) (labels k)))
+-- | Find metrics by `label`
+hasLabel :: String -> M.Map MetricId a -> M.Map MetricId a
+hasLabel label' = M.filterWithKey (\k _ -> (M.member (B.pack label') (labels k)))
 
-byLabel label contents = M.filterWithKey (\k _ -> (
-  case M.lookup (B.pack label) (labels k) of
+-- | Find metrics with `label` which matches `contents`
+byLabel :: String -> B.ByteString -> M.Map MetricId a -> M.Map MetricId a
+byLabel label' contents = M.filterWithKey (\k _ -> (
+  case M.lookup (B.pack label') (labels k) of
     Nothing -> False
     Just lc -> lc == contents
   ))
 
-byLabel' label op = M.filterWithKey (\k _ -> (
-  case M.lookup (B.pack label) (labels k) of
+-- | Find metrics with `label` which content satisfies `op` predicate
+byLabel' :: String -> (B.ByteString -> Bool) -> M.Map MetricId a -> M.Map MetricId a
+byLabel' label' op = M.filterWithKey (\k _ -> (
+  case M.lookup (B.pack label') (labels k) of
     Nothing -> False
     Just lc -> op lc
   ))
--- src/Data/Prometheus/Pretty.hs
+++ src/Data/Prometheus/Pretty.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Data.Prometheus.Pretty where
 
+import Data.String (IsString)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Map as M
 
@@ -20,22 +22,24 @@ prettyMetricShort :: MetricId -> Metric -> B.ByteString
 prettyMetricShort mId mData = B.unwords [ prettyId mId , prettyData mData ]
 
 prettyHelp :: MetricId -> B.ByteString
-prettyHelp (MetricId name help _) = B.unwords [ "# HELP", name, help ]
+prettyHelp MetricId{..} = B.unwords [ "# HELP", name, help ]
 
 prettyType :: MetricId -> Metric -> B.ByteString
 prettyType mId x = B.unwords [ "# TYPE", name mId, toTypeStr x ]
 
+toTypeStr :: IsString p => Metric -> p
 toTypeStr (Counter _)   = "counter"
 toTypeStr (Gauge _)     = "gauge"
 toTypeStr (Summary _ _ _)   = "summary"
 toTypeStr (Histogram _ _ _) = "histogram"
 
+prettyData :: Metric -> B.ByteString
 prettyData (Counter c) = B.pack $ show c
 prettyData (Gauge g) = B.pack $ show g
 prettyData _ = error "Unable to print summary or histogram"
 
 prettyId :: MetricId -> B.ByteString
-prettyId (MetricId name help labels) = B.concat [ name, prettyLabels labels ]
+prettyId MetricId{..} = B.concat [ name, prettyLabels labels ]
 
 prettyLabels :: Labels -> B.ByteString
 prettyLabels labels | M.null labels = ""
